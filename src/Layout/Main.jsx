bc1ebbd2e695145dc68f364b405b65574f4b85df
“chandrasree” <chandrasree097@gmail.com>
2024-06-06 13:00:25 +0600

    route setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom"
+import Footer from "../component/Footer"
+const Main = () => {
+  return (
+    <div>
+    <Outlet></Outlet>
+    <Footer></Footer>
+    </div>
+  )
+}
+
+export default Main
